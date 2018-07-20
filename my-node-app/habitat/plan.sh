b80f1c4e0f6addf8460aaae3de11d0f87b6bded4
anthonygrees <anthony_rees01@hotmail.com>
2018-07-20 21:55:43 +1000

    Hab hooks added

---
+++ my-node-app/habitat/plan.sh
@@ -0,0 +1,43 @@
+pkg_name=my-node-app
+pkg_origin=anthonyrees
+pkg_version="0.1.0"
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_license=('Apache-2.0')
+# pkg_source="http://some_source_url/releases/${pkg_name}-${pkg_version}.tar.gz"
+# pkg_filename="${pkg_name}-${pkg_version}.tar.gz"
+# pkg_shasum="TODO"
+pkg_deps=(core/node)
+pkg_build_deps=(core/make core/gcc)
+# pkg_lib_dirs=(lib)
+# pkg_include_dirs=(include)
+# pkg_bin_dirs=(bin)
+# pkg_pconfig_dirs=(lib/pconfig)
+# pkg_svc_run="bin/haproxy -f $pkg_svc_config_path/haproxy.conf"
+# pkg_exports=(
+#   [host]=srv.address
+#   [port]=srv.port
+#   [ssl-port]=srv.ssl.port
+# )
+# pkg_exposes=(port ssl-port)
+# pkg_binds=(
+#   [database]="port host"
+# )
+# pkg_binds_optional=(
+#   [storage]="port host"
+# )
+# pkg_interpreters=(bin/bash)
+# pkg_svc_user="hab"
+# pkg_svc_group="$pkg_svc_user"
+# pkg_description="Some description."
+# pkg_upstream_url="http://example.com/project-name"
+
+pkg_exports=( [port]=listening_port )
+pkg_exposes=(port)
+
+do_build() {
+    cp -vr $PLAN_CONTEXT/../app.js $CACHE_PATH
+}
+
+do_install() {
+    cp $CACHE_PATH/app.js ${pkg_prefix}
+}
